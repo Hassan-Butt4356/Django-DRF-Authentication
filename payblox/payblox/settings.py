9807418e15985e355486f55b30bee54b77d80f5f
Hassan Butt <itshassanbutt@gmail.com>
2022-11-02 13:29:56 -0700

    Final Commit

--- payblox/payblox/settings.py
+++ payblox/payblox/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,10 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i*e4s2!#^q(26^!!@tz0=)=k-nq4k73&3g@(bel-f-9#)a06-0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
@@ -120,11 +120,11 @@ LOGIN_REDIRECT_URL = 'accounts:home'
 LOGOUT_REDIRECT_URL = 'accounts:login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'itshassanbutt@gmail.com'
-EMAIL_HOST_PASSWORD = 'begfovwdheovqgsc'
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_PORT=config('EMAIL_PORT',cast=int)
+EMAIL_USE_TLS=config('EMAIL_USE_TLS',cast=bool)
+EMAIL_HOST_USER=config('EMAIL_HOST_USER',default='')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD',default='')
 
 
 REST_FRAMEWORK = {
